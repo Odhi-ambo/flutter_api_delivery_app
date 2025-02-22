e22e44907c44ca5d871b73ccd041dae312f546a9
Odhi-ambo <michaelderrick605@gmail.com>
2025-02-22 22:16:36 +0300

    pages folder

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _onboardingState();
+}
+
+class _onboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
