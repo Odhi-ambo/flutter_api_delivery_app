b5637f71c83d9396290077e79158a875e130fc56
Odhi-ambo <michaelderrick605@gmail.com>
2025-02-22 22:18:12 +0300

    initial commit for onboarding page

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -10,6 +10,6 @@ class Onboarding extends StatefulWidget {
 class _onboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
