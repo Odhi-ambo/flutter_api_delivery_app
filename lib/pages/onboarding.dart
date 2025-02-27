5c3ef275e6c5e20463249357a4d78031b7d0e085
Odhi-ambo <michaelderrick605@gmail.com>
2025-02-27 11:52:58 +0300

    image styling

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -15,7 +15,7 @@ class _onboardingState extends State<Onboarding> {
       body: Container(
         child: Column(
           children: [Image.asset('images/images (1).jpeg'),
-          Text('Explore \nthe best products', style: TextStyle(),)],
+          Text('Explore \nthe best products', style: TextStyle(color: Colors.black),)],
         ),
       ),
     );
