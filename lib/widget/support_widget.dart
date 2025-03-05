951fe94d3ac766f590313b68af8a848301d37208
Odhi-ambo <michaelderrick605@gmail.com>
2025-03-05 11:46:33 +0300

    state mngmt

--- lib/widget/support_widget.dart
+++ lib/widget/support_widget.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AppWidget{
-  static TextStyle boldTextFieldStyle(){
+class AppWidget {
+  static TextStyle boldTextFieldStyle() {
     return TextStyle(
-      color: Colors.black,
-      fontSize: 20,
-      fontWeight: FontWeight.bold
-    );
+        color: Colors.black, fontSize: 27, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle lightTextFieldStyle() {
+    return TextStyle(
+        color: Colors.black, fontSize: 20, fontWeight: FontWeight.normal);
   }
 }
