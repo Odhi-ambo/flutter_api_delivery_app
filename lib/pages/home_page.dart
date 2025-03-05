6f650c554f0fe5e5f4ae6a0d20c18e312a7691a2
Odhi-ambo <michaelderrick605@gmail.com>
2025-03-05 14:19:17 +0300

    homepage file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:deli_app/widget/support_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,13 +14,27 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: const EdgeInsets.only(top: 50, left: 20),
+        margin: const EdgeInsets.only(top: 50, left: 20, right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Hey, Rebecca!",
-              style: AppWidget.boldTextFieldStyle(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Hey, Rebecca!",
+                  style: AppWidget.boldTextFieldStyle(),
+                ),
+                ClipRRect(
+                  
+                  child: Image.asset(
+                    "assets/images/becky.jpg",
+                    height: 50,
+                    width: 50,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
             ),
             Text(
               'How You?!',
