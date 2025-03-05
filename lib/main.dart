01a2106a70ca78afc36cccbd968d727f3cd9202a
Odhi-ambo <michaelderrick605@gmail.com>
2025-03-05 11:14:12 +0300

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:deli_app/pages/home_page.dart';
 import 'package:deli_app/pages/onboarding.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Onboarding(),
+      home: const HomePage(),
     );
   }
 }
