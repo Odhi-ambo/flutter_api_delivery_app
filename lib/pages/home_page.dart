9c511a34fd4cd18d86e39b02ec64b1a702a754d2
Odhi-ambo <michaelderrick605@gmail.com>
2025-03-05 11:46:18 +0300

    homepage file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,10 +23,7 @@ class _HomePageState extends State<HomePage> {
             ),
             Text(
               'How You?!',
-              style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold),
+              style: AppWidget.lightTextFieldStyle(),
             )
           ],
         ),
